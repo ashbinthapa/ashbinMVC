aa321b1051d10f5b0e8853a02314ae44a3b143be
Asmit Thapa <thapasmit82@gmail.com>
2022-11-23 14:18:20 +0545

    first commit

---
+++ AshbinMVCWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AshbinMVCWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,1000,ErrorMessage ="Display Order must be between 1 to 1000.")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now; //present created date-time is added
+    }
+}
